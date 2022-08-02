eddc815f9cd4eb39663c5135e1c07259bc1ade28
wyliemickelson <wyliecoyote910@gmail.com>
2022-08-02 13:56:06 -0700

    edited

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { StyledHeader } from './styles/Header.style'
+
+export default function Header() {
+  return (
+    <StyledHeader bg="red">
+      <h1>Huddle</h1>
+    </StyledHeader>
+    
+  )
+}
